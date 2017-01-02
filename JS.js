6f326f3a17de9e02f66a15cd7f3a32da5042e79b
OmicronPersei <wheeler.weise@gmail.com>
2017-01-02 14:29:45 -0600

    Initial commit of detecting viewport size changing.

--- JS.js
+++ JS.js
@@ -1,3 +1,29 @@
-/*global $, jQuery*/
+/*global $, jQuery, ResponsiveBootstrapToolkit*/
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, white: true  */
 
-//('#mainPageContents').scrollspy();
+
+(function ($, document, window, viewport) {
+  
+  "use strict";
+  
+  var setAboutStyleClass = function (viewportSize) {
+    if (viewportSize === "xs") {
+      $('.aboutContentItem').removeClass('aboutRightJustify');
+      $('.aboutContentItem').addClass('aboutCenterJustify');
+    }
+    else {
+      $('.aboutContentItem').removeClass('aboutCenterJustify');
+      $('.aboutContentItem').addClass('aboutRightJustify');
+    }
+  };
+  
+  $(window).ready(function () {
+    setAboutStyleClass(viewport.current());
+  });
+  
+  $(window).resize(function () {
+    console.log('Current viewport size: ', viewport.current());
+    setAboutStyleClass(viewport.current());
+  });
+  
+}(jQuery, document, window, ResponsiveBootstrapToolkit));
